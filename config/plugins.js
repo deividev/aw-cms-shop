e4d14360aca29efbeefaa95a8c7d1bc5f81e40ff
David seco <deivid.develop@gmail.com>
2022-06-01 22:19:08 +0200

    fix: config vercel

--- config/plugins.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-    "vercel-deploy": {
-      enabled: true,
-    },
-    config: {
-        deployHook:"https://api.vercel.com/v1/integrations/deploy/prj_RB2XJlqkOoGUwIqsKy9HoBGZKUOM/OLEyqbgvgK",
-        apiToken: "hRZsEFMYNTYWCPi4IzNqrTOM",
-      },
-  });
