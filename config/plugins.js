06bd9d51123efd1b2070ca70faef9787fe3610b2
David seco <deivid.develop@gmail.com>
2022-06-02 19:04:21 +0200

    Revert "fix: config vercel"
    This reverts commit e4d14360aca29efbeefaa95a8c7d1bc5f81e40ff.

---
+++ config/plugins.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+      enabled: true,
+    },
+    config: {
+        deployHook:"https://api.vercel.com/v1/integrations/deploy/prj_RB2XJlqkOoGUwIqsKy9HoBGZKUOM/OLEyqbgvgK",
+        apiToken: "hRZsEFMYNTYWCPi4IzNqrTOM",
+      },
+  });
--- config/server.js
+++ config/server.js
@@ -4,16 +4,4 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  vercel: {
-    // Required
-    token: env('hRZsEFMYNTYWCPi4IzNqrTOM'),
-    // Required
-    projectId: env('VERCEL_PROJECT_ID'),
-    // Optional (required if you use teams)
-    teamId: env('prj_RB2XJlqkOoGUwIqsKy9HoBGZKUOM'),
-    // Required (hooks)
-    triggers: {
-        production: env('https://api.vercel.com/v1/integrations/deploy/prj_RB2XJlqkOoGUwIqsKy9HoBGZKUOM/OLEyqbgvgK')
-    },
-  }
 });
