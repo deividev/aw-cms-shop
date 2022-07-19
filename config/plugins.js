0601e695659e490967c40a8c3cf6163b0adc4f7e
Alejandro <alejandro.m.gomez.92@gmail.com>
2022-07-19 21:04:34 +0200

    add config to use spaces

--- config/plugins.js
+++ config/plugins.js
@@ -6,4 +6,15 @@ module.exports = ({ env }) => ({
         deployHook:"https://api.vercel.com/v1/integrations/deploy/prj_RB2XJlqkOoGUwIqsKy9HoBGZKUOM/OLEyqbgvgK",
         apiToken: "hRZsEFMYNTYWCPi4IzNqrTOM",
       },
+      upload: {
+        config: {
+          provider: "strapi-provider-upload-dos",
+          providerOptions: {
+            key: process.env.DO_SPACE_ACCESS_KEY,
+            secret: process.env.DO_SPACE_SECRET_KEY,
+            endpoint: process.env.DO_SPACE_ENDPOINT,
+            space: process.env.DO_SPACE_BUCKET,
+          },
+        },
+      },
   });
