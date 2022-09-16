fe55d072587624316be69b81ddaf65b1537a5963
David seco <deivid.develop@gmail.com>
2022-09-16 09:10:31 +0200

    feat: list-grammage and relation to products

---
+++ src/api/list-grammage/routes/list-grammage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * list-grammage router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::list-grammage.list-grammage');
