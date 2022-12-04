8284717dc9eefcab2f53b412314fdfc4508ada8e
David seco <deivid.develop@gmail.com>
2022-12-04 15:36:34 +0100

    Feat: create infoFooter and legalFooter colletions

---
+++ src/api/legal-footer/services/legal-footer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * legal-footer service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::legal-footer.legal-footer');
