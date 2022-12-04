90f19eaf56e7278a7e81d34273808126882c77c3
David seco <deivid.develop@gmail.com>
2022-12-04 10:25:01 +0100

    Fix: generic list for properties products

--- src/api/list-grammage/services/list-grammage.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * list-grammage service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::list-grammage.list-grammage');
