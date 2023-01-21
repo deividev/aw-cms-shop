9a755a894fa4fac11e2d512fbc61e6685ee4fc61
David seco <deivid.develop@gmail.com>
2023-01-21 12:02:48 +0100

    Feat: create home content collections

---
+++ src/api/home-link/controllers/home-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  home-link controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-link.home-link');
---
+++ src/api/home-list/controllers/home-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  home-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-list.home-list');
