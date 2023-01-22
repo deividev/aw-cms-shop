9537c473519880501f6fe1c4ff89262364b4e1e1
David seco <deivid.develop@gmail.com>
2023-01-22 22:33:55 +0100

    Fix: clean unused collections

--- src/api/properties-product/routes/properties-product.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * properties-product router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::properties-product.properties-product');
